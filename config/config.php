832fbc9a2d2fee3ba1a0c29af7e54ad8e645d627
Eduardo Soares Ferreira <46054482+Eduardosfer@users.noreply.github.com>
2020-07-14 15:29:51 -0300

    Removido espaços em branco pra evitar problema com a função header

--- config/config.php
+++ config/config.php
@@ -1,20 +1,15 @@
 <?php
-
-/* 
+/*
  * CONFIG OPTIONS
  */
-
 //ENDEREÇO EX: header("Location: ".BASE_URL_ALG."visao/principal.php");
 define("BASE_URL_ALG",  "/algtot/");
-
 //BANCO DE DADOS
 define("LOCAL_HOST",  "localhost");
 define("DB_NAME",  "algtot");
 define("DB_USER_NAME",  "root");
 define("DB_USER_SENHA",  "root");
-
 //CONFIGURAÇÕES DE DATA/HORA
 date_default_timezone_set("America/Bahia");
-
 //CONFIGURAÇÕES DE CHARSET
 header('Content-Type: text/html; charset=UTF-8');
